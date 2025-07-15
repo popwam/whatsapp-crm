88e37f26bcd11cdbc8628620209e86377ff23739
popwam <mmdohsaad39@gmail.com>
2025-07-15 18:52:05 +0300

    تحديث الكود بعد التعديلات الأخيرة

--- app.py
+++ app.py
@@ -335,4 +335,4 @@ def send_bulk():
     return render_template("send_bulk_result.html", results=results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
